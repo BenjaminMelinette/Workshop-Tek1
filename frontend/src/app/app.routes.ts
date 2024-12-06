5961095e2b326a7dfa94e8750814daa9c1b0a5ce
BenjaminMelinette <benjamin.melinette@epitech.eu>
2024-12-06 09:11:52 +0100

    [ADD] Frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TabComponent } from './tab/tab.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "tab", component: TabComponent}
+];
--- frontend/src/app/tab/tab.component.ts
+++ frontend/src/app/tab/tab.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { User } from './user.model';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-tab',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatDialogModule],
   templateUrl: './tab.component.html',
   styleUrl: './tab.component.css'
 })
-export class TabComponent {
+export class TabComponent implements OnInit{
+  displayedColumns = ["select", "photo", "nom", "email"]
+  users: User[] = [];
+  userInfo: {photo: string, name: string, email: string} | null = null;
+  dataSource = new MatTableDataSource<User>(this.users);
+  selection = new SelectionModel<User>(true, []);
 
+  constructor(){}
+
+  ngOnInit(){
+    this.users = [
+      {name: "Test", email: "Test", id: 0, photo: "https://img.icons8.com/ios-glyphs/50/user--v1.png"},
+      {name: "Test 2", email: "Test 2", id: 1, photo: "https://img.icons8.com/ios-glyphs/50/user--v1.png"},
+    ]
+    this.dataSource.data = this.users;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    
+    this.dataSource.filterPredicate = (data: User, filter: string) => {
+      const dataStr = `${data.name} ${data.email}`.toLowerCase();
+      return dataStr.includes(filter);
+    };
+  
+    this.dataSource.filter = filterValue;
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  user_lenght(){
+    return this.users.length;
+  }
+
+  showPopup(){
+    return;
+  }
 }
